d142aac43e9b70d6ecf21b11495b8e68710343ab
Marcin Malessa <marcin@malessa.pl>
2022-11-12 22:23:45 +0100

    Add symfony/monolog

--- src/Waiter/Application/MessageHandler/PlaceOrderHandler.php
+++ src/Waiter/Application/MessageHandler/PlaceOrderHandler.php
@@ -6,23 +6,26 @@ namespace App\Waiter\Application\MessageHandler;
 
 use App\Waiter\Application\Message\Waiter\Command\PlaceOrder;
 use App\Waiter\Application\Message\Waiter\Event\OrderPlaced;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class PlaceOrderHandler implements MessageHandlerInterface
 {
     public function __construct(
-        private readonly MessageBusInterface $messageBus
+        private readonly MessageBusInterface $messageBus,
+        private readonly LoggerInterface $logger
     )
     {
     }
 
     public function __invoke(PlaceOrder $command)
     {
-        echo "PlaceOrderHandler\n";
+        $this->logger->info("PlaceOrderHandler");
         $this->messageBus->dispatch(new OrderPlaced(
             $command->tableId,
             $command->order
         ));
+        $this->logger->info("PlaceOrderHandler Done");
     }
 }

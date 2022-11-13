1be0a11add31f77c6a5f80fac2ef8ab521fc7962
Marcin Malessa <marcin@malessa.pl>
2022-11-13 17:52:35 +0100

    Modify PlaceOrder command

--- src/Waiter/UI/Command/PlaceOrderCommand.php
+++ src/Waiter/UI/Command/PlaceOrderCommand.php
@@ -3,7 +3,6 @@
 namespace App\Waiter\UI\Command;
 
 use App\Waiter\Application\Message\Waiter\Command\PlaceOrder;
-use App\Waiter\Application\Message\Waiter\Event\OrderPlaced;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;

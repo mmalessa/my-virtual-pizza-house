b833d33342eeac7aa7bef01eb53541acc6d966f9
Marcin Malessa <marcin@malessa.pl>
2022-11-18 22:38:29 +0100

    Fix OrderManager OrderPlaced

--- src/OrderManager/Application/Message/Waiter/Event/OrderPlaced.php
+++ src/OrderManager/Application/Message/Waiter/Event/OrderPlaced.php
@@ -12,5 +12,19 @@ class OrderPlaced implements WaiterMessageInterface
         public readonly string $timestamp
     )
     {
+        if ($this->tableId === '') {
+            throw new \InvalidArgumentException('$tableId cannot be empty');
+        }
+        if ($this->order === []) {
+            throw new \InvalidArgumentException('$order cannot be empty');
+        }
+        if ($this->timestamp === '') {
+            throw new \InvalidArgumentException('$timestamp cannot be empty');
+        }
+        $dateFormat = 'Y-m-d H:i:s';
+        $d = \DateTime::createFromFormat($dateFormat, $this->timestamp);
+        if ($d->format($dateFormat) !== $this->timestamp) {
+            throw new \InvalidArgumentException('$timestamp has invalid format');
+        }
     }
 }

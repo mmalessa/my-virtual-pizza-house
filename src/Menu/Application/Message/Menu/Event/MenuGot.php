2d9885b45cb96f360621131209bb979a8de8b1b4
Marcin Malessa <marcin@malessa.pl>
2023-02-17 20:32:15 +0100

    Modify message/event in Menu + tests

--- src/Menu/Application/Message/Menu/Event/MenuGot.php
+++ src/Menu/Application/Message/Menu/Event/MenuGot.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\Menu\Application\Message\Menu\Event;
 
 use App\Menu\Application\Message\MenuMessageInterface;
+use Ramsey\Uuid\Uuid;
 
 readonly class MenuGot  implements MenuMessageInterface
 {
     public function __construct(public string $sagaId, public array $menu)
     {
+        if (empty($this->sagaId) || !Uuid::isValid($this->sagaId)) {
+            throw new \InvalidArgumentException("SagaId cannot be empty and must be UUID(v4)");
+        }
     }
 }
---
+++ tests/Unit/Menu/Application/Message/Menu/Command/GetMenuTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Menu\Application\Message\Menu\Command;
+
+use App\Menu\Application\Message\Menu\Command\GetMenu;
+use PHPUnit\Framework\TestCase;
+
+class GetMenuTest extends TestCase
+{
+    /** @dataProvider provideValidData */
+    public function testGetMenu(string $sagaId)
+    {
+        $getMenu = new GetMenu($sagaId);
+        $this->assertEquals($sagaId, $getMenu->sagaId);
+    }
+
+    private function provideValidData(): array
+    {
+        return [
+            ['8ed543d7-a6ad-4746-8c11-7b27437c0d38'],
+            ['c9c18f74-4f08-4cd4-bdaa-702bdc5594b9'],
+        ];
+    }
+
+    /** @dataProvider provideInvalidData */
+    public function testGetMenuError(string $sagaId)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new GetMenu($sagaId);
+    }
+
+    private function provideInvalidData(): array
+    {
+        return [
+            [''],
+            ['c9c18f74-4f0-4cd4-bdaa-702bdc5594b9'],
+            ['64821cee-659f'],
+        ];
+    }
+}
